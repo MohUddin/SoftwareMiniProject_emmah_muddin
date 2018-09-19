2841135ba366497a72cfa82c7b4eff756da62ae7
Emma Howard <emmahoward@Emmas-MacBook-Pro.local>
2018-09-19 11:40:53 -0400

    Revisions for database interaction

---
+++ xCode Project/SWMiniProject/ChartsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChartsViewController.swift
+//  SWMiniProject
+//
+//  Created by Emma Howard on 9/19/18.
+//  Copyright © 2018 Emma Howard. All rights reserved.
+//
+
+import Foundation
---
+++ xCode Project/SWMiniProject/House.swift
@@ -0,0 +1,9 @@
+//
+//  House.swift
+//  SWMiniProject
+//
+//  Created by Emma Howard on 9/19/18.
+//  Copyright © 2018 Emma Howard. All rights reserved.
+//
+
+import Foundation
