ace26485aaf154e628a9cb8873474f6520637d1c
MohUddin <muddin1@bu.edu>
2018-09-20 10:20:02 -0400

    small changes to Database update

--- main.py
+++ main.py
@@ -9,29 +9,7 @@ default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-# # generating random numbers for humidity and temperature
-#
-# #randomData = {
-# #        humidity = 5
-# #      "temperature" = 33#     }
-#
-# def get_quote() ->
-#     {
-#         'author' :
-#     }
-#
-# #
-# def get_data():
-#     generate_data(x)
-#     dat = json.dumps(x)
-#     return dat
 
-
-# getting a response to post on database
-#
-# response = get_data()
-# temperature = response.body['temperature']
-# humidity = response.body['humidity']
 
 
 
@@ -51,7 +29,7 @@ for x in range(10):
 
 u = 'u'
 
-for i in range(50):
+for i in range(25):
     house_id = ('testID' + str(i))
     print(house_id)
 
@@ -65,6 +43,12 @@ doc_ref_users = db.collection(u'Users').document(u'testUser')
 doc_ref_users.set({
     u'email': u'muddin1@bu.edu',
     u'test_field': hum1
+})
+
+doc_ref = db.collection(u'Houses').document(u'KjfrMD8eXilnJJ0n9CuO')
+doc_ref.set({
+    u'humidity': hum1,
+    u'temperature': temp1
 })
 
 
